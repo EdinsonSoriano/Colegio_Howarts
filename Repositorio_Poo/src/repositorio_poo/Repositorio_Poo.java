0ba5353a129ab86b37e15c539b8d65f353c89e27
EdinsonIsHere <sir.sored@gmail.com>
2017-11-19 13:37:29 -0500

    NombreRepositorio
    Muestra un mensaje que se debe cambiar el nombre del repositorio
    Por ejemplo: ColegioHowardsESPOL

--- Repositorio_Poo/src/repositorio_poo/Repositorio_Poo.java
+++ Repositorio_Poo/src/repositorio_poo/Repositorio_Poo.java
@@ -15,6 +15,7 @@ public class Repositorio_Poo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hay que cambiarle el nombre al repositorio pot Colegio Hogwards");
         // TODO code application logic here
     }
     
